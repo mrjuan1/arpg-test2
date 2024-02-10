d31fe48c48914ddaca62c6ad7d00860565f1feb5
Juan Wolfaardt <juanwolfaardt@gmail.com>
2024-02-11 00:22:37 +0200

    Add initial attacking logic

--- Character.gd
+++ Character.gd
@@ -34,6 +34,24 @@ extends CharacterBody3D
 @export var dodging_min_directional_speed: float = Dodging.DEFAULT_MIN_DIRECTIONAL_SPEED
 @export var dodging_stationary_air_dampening: float = Dodging.DEFAULT_STATIONARY_AIR_DAMPENING
 
+@export_group("Melee")
+@export var combo_timer: Timer
+@export_subgroup("Strength")
+@export var initial_strength: float = Melee.DEFAULT_INITIAL_STRENGTH
+@export var initial_strength_reduction_factor: float = Melee.DEFAULT_INITIAL_STRENGTH_REDUCTION_FACTOR
+@export var strength_reduction_decrement: float = Melee.DEFAULT_STRENGTH_REDUCTION_DECREMENT
+@export_subgroup("Charge rate")
+@export var initial_charge_rate: float = Melee.DEFAULT_INITIAL_CHARGE_RATE
+@export var initial_charge_rate_reduction_factor: float = Melee.DEFAULT_INITIAL_CHARGE_RATE_REDUCTION_FACTOR
+@export var charge_rate_reduction_decrement: float = Melee.DEFAULT_CHARGE_RATE_REDUCTION_DECREMENT
+@export_subgroup("Max charge")
+@export var initial_max_charge: float = Melee.DEFAULT_INITIAL_MAX_CHARGE
+@export var initial_max_charge_reduction_factor: float = Melee.DEFAULT_INITIAL_MAX_CHARGE_REDUCTION_FACTOR
+@export var max_charge_reduction_decrement: float = Melee.DEFAULT_MAX_CHARGE_REDUCTION_DECREMENT
+@export_subgroup("")
+@export var release_strength_cutoff: float = Melee.DEFAULT_RELEASE_STRENGTH_CUTOFF
+@export var max_combos: int = Melee.DEFAULT_MAX_COMBOS
+
 @export_group("Camera")
 @export var camera: Camera3D
 @export_subgroup("Lerp speeds")
@@ -44,9 +62,10 @@ extends CharacterBody3D
 @export var camera_y_negative_follow_distance: float = PlayerCamera.DEFAULT_CAMERA_Y_NEGATIVE_FOLLOW_DISTANCE
 #endregion exports
 
-#region variables
 var input_vec: Vector2 = Vector2.ZERO
+var charging_melee: bool = false
 
+#region onready
 @onready var movement: Movement = Movement.new(
 	self,
 	gravity_multiplier,
@@ -85,6 +104,25 @@ var input_vec: Vector2 = Vector2.ZERO
 	dodging_stationary_air_dampening,
 )
 
+@onready var melee: Melee = Melee.new(
+	combo_timer,
+
+	initial_strength,
+	initial_strength_reduction_factor,
+	strength_reduction_decrement,
+
+	initial_charge_rate,
+	initial_charge_rate_reduction_factor,
+	charge_rate_reduction_decrement,
+
+	initial_max_charge,
+	initial_max_charge_reduction_factor,
+	max_charge_reduction_decrement,
+
+	release_strength_cutoff,
+	max_combos,
+)
+
 @onready var player_camera: PlayerCamera = PlayerCamera.new(
 	self,
 	camera,
@@ -95,23 +133,40 @@ var input_vec: Vector2 = Vector2.ZERO
 	camera_y_positive_follow_distance,
 	camera_y_negative_follow_distance,
 )
-#endregion variables
+
+func _ready() -> void:
+	melee.attack_released.connect(_on_melee_attack_released)
+#endregion onready
 
 #region processing
-func _process(_delta: float) -> void:
-	input_vec = Input.get_vector("left", "right", "up", "down")
-	if input_vec:
-		movement.set_direction(input_vec)
-		movement.moving = true
-	else:
-		movement.moving = false
+func _process(delta: float) -> void:
+	if charging_melee:
+		var charge_state: Melee.ChargeState = melee.charge_attack(delta)
 
-	if Input.is_action_just_pressed("jump"):
-		jumping.jump()
-	elif Input.is_action_just_pressed("dodge"):
-		dodging.dodge()
+		if charge_state == Melee.ChargeState.CHARGING and Input.is_action_just_released("attack"):
+			melee.release_attack()
+			charging_melee = false
+		elif charge_state == Melee.ChargeState.RELEASED or charge_state == Melee.ChargeState.COOLING_OFF:
+			charging_melee = false
+	else:
+		input_vec = Input.get_vector("left", "right", "up", "down")
+		if input_vec:
+			movement.set_direction(input_vec)
+			movement.moving = true
+		else:
+			movement.moving = false
+
+		if Input.is_action_just_pressed("jump"):
+			jumping.jump()
+		elif Input.is_action_just_pressed("dodge"):
+			dodging.dodge()
+		elif Input.is_action_just_pressed("attack"):
+			charging_melee = true
 
 func _physics_process(delta: float) -> void:
 	movement.physics_process(delta)
 	player_camera.physics_process(delta)
 #endregion processing
+
+func _on_melee_attack_released(strength: float, combo: int) -> void:
+	prints("Melee attack released with strength", strength, "at combo", combo)
---
+++ Melee.gd
@@ -0,0 +1,154 @@
+class_name Melee
+
+signal attack_released
+
+enum ChargeState {
+	CHARGING,
+	RELEASED,
+	COOLING_OFF,
+}
+
+#region constants
+const DEFAULT_INITIAL_STRENGTH: float = 3.0
+const DEFAULT_INITIAL_STRENGTH_REDUCTION_FACTOR: float = 0.9
+const DEFAULT_STRENGTH_REDUCTION_DECREMENT: float = 0.1
+
+const DEFAULT_INITIAL_CHARGE_RATE: float = 0.25
+const DEFAULT_INITIAL_CHARGE_RATE_REDUCTION_FACTOR: float = 0.9
+const DEFAULT_CHARGE_RATE_REDUCTION_DECREMENT: float = 0.1
+
+const DEFAULT_INITIAL_MAX_CHARGE: float = 0.5
+const DEFAULT_INITIAL_MAX_CHARGE_REDUCTION_FACTOR: float = 0.9
+const DEFAULT_MAX_CHARGE_REDUCTION_DECREMENT: float = 0.1
+
+const DEFAULT_RELEASE_STRENGTH_CUTOFF: float = 0.1
+
+const DEFAULT_MAX_COMBOS: int = 3
+#endregion constants
+
+#region privates
+var _combo_timer: Timer
+
+var _initial_strength: float
+var _initial_strength_reduction_factor: float
+var _strength_reduction_decrement: float
+
+var _initial_charge_rate: float
+var _initial_charge_rate_reduction_factor: float
+var _charge_rate_reduction_decrement: float
+
+var _initial_max_charge: float
+var _initial_max_charge_reduction_factor: float
+var _max_charge_reduction_decrement: float
+
+var _release_strength_cutoff: float
+var _max_combos: int
+
+var _may_charge: bool = true
+var _combo: int = 0
+var _charge: float = 0.0
+
+var _strength: float
+var _strength_reduction_factor: float
+
+var _charge_rate: float
+var _charge_rate_reduction_factor: float
+
+var _max_charge: float
+var _max_charge_reduction_factor: float
+#endregion privates
+
+#region constructor
+func _init(
+	combo_timer: Timer,
+
+	initial_strength: float = DEFAULT_INITIAL_STRENGTH,
+	initial_strength_reduction_factor: float = DEFAULT_INITIAL_STRENGTH_REDUCTION_FACTOR,
+	strength_reduction_decrement: float = DEFAULT_STRENGTH_REDUCTION_DECREMENT,
+
+	initial_charge_rate: float = DEFAULT_INITIAL_CHARGE_RATE,
+	initial_charge_rate_reduction_factor: float = DEFAULT_INITIAL_CHARGE_RATE_REDUCTION_FACTOR,
+	charge_rate_reduction_decrement: float = DEFAULT_CHARGE_RATE_REDUCTION_DECREMENT,
+
+	initial_max_charge: float = DEFAULT_INITIAL_MAX_CHARGE,
+	initial_max_charge_reduction_factor: float = DEFAULT_INITIAL_MAX_CHARGE_REDUCTION_FACTOR,
+	max_charge_reduction_decrement: float = DEFAULT_MAX_CHARGE_REDUCTION_DECREMENT,
+
+	release_strength_cutoff: float = DEFAULT_RELEASE_STRENGTH_CUTOFF,
+	max_combos: int = DEFAULT_MAX_COMBOS,
+) -> void:
+	_combo_timer = combo_timer
+	_combo_timer.connect("timeout", _on_combo_timer_timeout)
+
+	_initial_strength = initial_strength
+	_initial_strength_reduction_factor = initial_strength_reduction_factor
+	_strength_reduction_decrement = strength_reduction_decrement
+
+	_initial_charge_rate = initial_charge_rate
+	_initial_charge_rate_reduction_factor = initial_charge_rate_reduction_factor
+	_charge_rate_reduction_decrement = charge_rate_reduction_decrement
+
+	_initial_max_charge = initial_max_charge
+	_initial_max_charge_reduction_factor = initial_max_charge_reduction_factor
+	_max_charge_reduction_decrement = max_charge_reduction_decrement
+
+	_release_strength_cutoff = release_strength_cutoff
+	_max_combos = max_combos
+#endregion constructor
+
+#region functions
+func _on_combo_timer_timeout() -> void:
+	if _combo > 0:
+		_combo = 0
+	else:
+		_may_charge = true
+
+func charge_attack(delta: float) -> ChargeState:
+	if _may_charge:
+		if not _combo_timer.is_stopped():
+			_combo_timer.stop()
+
+		if _combo == 0 and _charge == 0.0:
+			_strength = _initial_strength
+			_strength_reduction_factor = _initial_strength_reduction_factor
+
+			_charge_rate = _initial_charge_rate
+			_charge_rate_reduction_factor = _initial_charge_rate_reduction_factor
+
+			_max_charge = _initial_max_charge
+			_max_charge_reduction_factor = _initial_max_charge_reduction_factor
+
+		_charge += _charge_rate * delta
+		if _charge > _max_charge:
+			_charge = _max_charge
+			release_attack()
+			return ChargeState.RELEASED
+		else:
+			return ChargeState.CHARGING
+
+	return ChargeState.COOLING_OFF
+
+func release_attack() -> void:
+	var release_strength: float = _strength + _charge
+	if release_strength < _release_strength_cutoff:
+		release_strength = _release_strength_cutoff
+	attack_released.emit(release_strength, _combo)
+
+	_charge = 0.0
+
+	_combo += 1
+	if (_max_combos > 0 and _combo == _max_combos) or release_strength == _release_strength_cutoff:
+		_combo = 0
+		_may_charge = false
+	else:
+		_strength *= _strength_reduction_factor
+		_strength_reduction_factor -= _strength_reduction_decrement
+
+		_charge_rate *= _charge_rate_reduction_factor
+		_charge_rate_reduction_factor -= _charge_rate_reduction_decrement
+
+		_max_charge *= _max_charge_reduction_factor
+		_max_charge_reduction_factor -= _max_charge_reduction_decrement
+
+	_combo_timer.start()
+#endregion functions
